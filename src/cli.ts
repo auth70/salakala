3d6c0515250c16bf50cae09ff3de68a6d2c6054a
miunau <miunau@miunau.com>
2025-10-13 02:22:58 +0200

    bump versions

--- src/cli.ts
+++ src/cli.ts
@@ -84,7 +84,7 @@ async function promptForEnvironment(environments: string[]): Promise<string> {
     return selectedEnvironment;
 }
 
-const PACKAGE_VERSION = '1.2.0';
+const PACKAGE_VERSION = '1.2.1';
 
 program
     .name('salakala')
